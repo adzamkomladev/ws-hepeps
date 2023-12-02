252eb9721acaf680af00b1193a4aab1d50ce19c5
Komla Adzam <adzamkomla.dev@gmail.com>
2023-12-02 13:30:24 +0000

    Complete task

--- src/chat/chat.gateway.ts
+++ src/chat/chat.gateway.ts
@@ -1,35 +1,51 @@
 import { Logger } from '@nestjs/common';
-import { WebSocketGateway, WebSocketServer, SubscribeMessage, WsResponse, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit } from '@nestjs/websockets';
+import { WebSocketGateway, WebSocketServer, SubscribeMessage, OnGatewayConnection, OnGatewayDisconnect, OnGatewayInit, WsException } from '@nestjs/websockets';
 
 import { Server } from 'ws';
-import { Observable, from, map, of } from 'rxjs';
 
-import { ChatService } from './chat.service';
 import { BroadcastMessageDto } from './dto/broadcast.message.dto';
 
 @WebSocketGateway(40021)
 export class ChatGateway implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect {
   private readonly logger = new Logger(ChatGateway.name);
+  private readonly wsClients = [];
 
   @WebSocketServer()
   server: Server;
 
-  constructor(private readonly chatService: ChatService) { }
-
   afterInit(server: any): void {
-    this.logger.log('Init');
+    this.logger.log('Init websocket server');
   }
 
   handleDisconnect(client: any): void {
-    this.logger.log(`Client disconnected: ${client.id}`);
+    for (let i = 0; i < this.wsClients.length; i++) {
+      if (this.wsClients[i] === client) {
+        this.wsClients.splice(i, 1);
+        break;
+      }
+    }
+    this.broadcast({ event: 'disconnect' });
+    this.logger.log(`Client disconnected`);
   }
 
   handleConnection(client: any, ...args: any[]): void {
+    this.wsClients.push(client);
     this.logger.log(`Client connected: `,);
   }
 
   @SubscribeMessage('send-message')
-  create(client: any, data: BroadcastMessageDto): Observable<WsResponse<BroadcastMessageDto>> {
-    return of({ event: 'received-message', data });
+  sendMessage(client: any, data: BroadcastMessageDto) {
+    try {
+      this.broadcast({ event: 'received-message', data })
+    } catch (error) {
+      throw new WsException(error?.message);
+    }
+  }
+
+  private broadcast(message: any) {
+    const broadCastMessage = JSON.stringify(message);
+    for (let c of this.wsClients) {
+      c.send(broadCastMessage);
+    }
   }
 }
